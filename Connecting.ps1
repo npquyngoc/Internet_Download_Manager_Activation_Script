4d5b04f212072d7b9270c163ba5bf244d994ec3c
Nguyễn Ngọc <100895216+npquyngoc@users.noreply.github.com>
2022-08-29 02:14:45 +0700

    Update Connecting.ps1

--- Connecting.ps1
+++ Connecting.ps1
@@ -1,7 +1,7 @@
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = "https://raw.githubusercontent.com/Internet_Download_Manager_Activation_Script/blob/main/IDM_Activation_Script.cmd"
-$FilePath = "$env:TEMP\IAS.cmd"
+$DownloadURL = "https://raw.githubusercontent.com/npquyngoc/Internet_Download_Manager_Activation_Script/main/IDM_Activation_Script.cmd"
+$FilePath = "$env:TEMP\TEMP_IAS.cmd"
 
 try {
     Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
