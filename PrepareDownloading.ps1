40d3492297466837b1f60ba40cb595bf6401de28
Nguyễn Ngọc <100895216+npquyngoc@users.noreply.github.com>
2022-10-17 09:21:15 +0700

    Refactor and optimize code

--- PrepareDownloading.ps1
+++ PrepareDownloading.ps1
@@ -1,18 +1,21 @@
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+[System.Net.ServicePointManager] :: SecurityProtocol = [System.Net.ServicePointManager] :: SecurityProtocol -bor [System.Net.SecurityProtocolType] :: Tls12
 
 $DownloadURL = "https://raw.githubusercontent.com/npquyngoc/Internet_Download_Manager_Activation_Script/main/IAS_Offline.cmd"
 $FilePath = "$env:TEMP\TEMP_IAS.cmd"
 
-try {
+try
+{
     Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
-
-} catch {
-    Write-Error $_
-    Return
 }
 
-if (Test-Path $FilePath) {
+catch
+{
+    Write-Error $_
+    return
+}
+
+if (Test-Path $FilePath)
+{
     Start-Process $FilePath -Wait
-    $item = Get-Item -LiteralPath $FilePath
-    $item.Delete()
+    Get-Item -LiteralPath $FilePath.Delete()
 }
