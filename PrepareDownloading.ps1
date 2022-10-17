31181e1d6402872172e3327efd6919db06d4916b
Nguyễn Ngọc <100895216+npquyngoc@users.noreply.github.com>
2022-10-17 09:31:32 +0700

    Update PrepareDownloading.ps1

--- PrepareDownloading.ps1
+++ PrepareDownloading.ps1
@@ -1,7 +1,7 @@
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
 $DownloadURL = "https://raw.githubusercontent.com/npquyngoc/Internet_Download_Manager_Activation_Script/main/IAS_Offline.cmd"
-$FilePath = "$env:TEMP\TEMP_IAS.cmd"
+$FilePath = "$env:temp\Temp_IAS_Offline.cmd"
 
 try
 {
@@ -10,12 +10,11 @@ try
 
 catch
 {
-    Write-Error $_
-    return
+    Write-Error $_ return
 }
 
 if (Test-Path $FilePath)
 {
     Start-Process $FilePath -Wait
-    Get-Item -LiteralPath $FilePath.Delete()
+    (Get-Item -LiteralPath $FilePath).Delete()
 }
