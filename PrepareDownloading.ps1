698348b16c8e250298fa211e22d6d4b3d364c0d7
Nguyễn Ngọc <100895216+npquyngoc@users.noreply.github.com>
2022-10-17 09:43:43 +0700

    Update PrepareDownloading.ps1

--- PrepareDownloading.ps1
+++ PrepareDownloading.ps1
@@ -1,21 +1,23 @@
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12;
 
-$DownloadURL = "https://raw.githubusercontent.com/npquyngoc/Internet_Download_Manager_Activation_Script/main/IAS_Offline.cmd"
-$FilePath = "$env:temp\Temp_IAS_Offline.cmd"
+$DownloadURL = "https://raw.githubusercontent.com/npquyngoc/Internet_Download_Manager_Activation_Script/main/IAS_Offline.cmd";
+$FilePath = "$env:temp\Temp_IAS_Offline.cmd";
 
 try
 {
-    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
+    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath;
 }
 
 catch
 {
-    Write-Error $_
-    return
+    Write-Error $_;
+    return;
 }
 
 if (Test-Path $FilePath)
 {
-    Start-Process $FilePath -Wait
-    [Get-Item -LiteralPath $FilePath].Delete()
+    Start-Process $FilePath -Wait;
+
+    $FileItem = Get-Item -LiteralPath $FilePath;
+    $FileItem.Delete();
 }
